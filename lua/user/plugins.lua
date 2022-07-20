5f7d9ce7f307865262448dc4bada6954048beba4
Chuck Smith <chuck.smith@opscompass.com>
2022-07-20 07:14:37 -0400

    Finish setting up buffer-line

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -62,6 +62,8 @@ return packer.startup(function(use)
   use "numToStr/Comment.nvim"
   use "kyazdani42/nvim-web-devicons"
   use "kyazdani42/nvim-tree.lua" 
+  use "akinsho/bufferline.nvim"
+  use "moll/vim-bbye"
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
